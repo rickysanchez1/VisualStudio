2ba02411258eccf64d65cf4d5080deaa2518b145
rickysanchez1 <richard.sanchez1983@gmail.com>
2024-06-07 16:46:12 -0700

    Update

---
+++ WebAppTutorial/WebAppTutorial/wwwroot/js/site.js
@@ -0,0 +1,25 @@
+﻿//Functions to be called onmouseover and onmouseout to change the color of "Hello World"
+function newColor() {
+    document.getElementById("hello_world").style.color = "blue";
+}
+
+function oldColor() {
+    document.getElementById("hello_world").style.color = "darkolivegreen";
+}
+
+var id = null;
+function boxMove() {
+    var elem = document.getElementById("animation");
+    var pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
+    }
+}
